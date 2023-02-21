abba01b004e8a8cf349ac96c997b8f0a0130d029
Adham Amr <104816206+Adham-amr-1@users.noreply.github.com>
2023-02-21 22:22:44 +0200

    Update Sheet1(a).cpp

--- Sheet1(a).cpp
+++ Sheet1(a).cpp
@@ -3,6 +3,7 @@
 call a function to add these two numbers and call another function
 to sub these numbers using local and global variables.
 */
+// using local variable
 #include <iostream>
 using namespace std;
 void sum(int, int);
@@ -22,6 +23,34 @@ void sum(int a, int b) {
 }
 void sub(int a, int b) {
     cout << "Subtraction of the 2 numbers = " << a - b;
+}
+//*****************************************************************************
+
+/*
+1.	Write a program that reads two integer numbers from a user and
+call a function to add these two numbers and call another function
+to sub these numbers using local and global variables.
+*/
+// using global variable
+#include <iostream>
+using namespace std;
+void sum(int, int);
+void sub(int, int);
+int x, y;
+int main()
+{
+    cout << "Enter 2 Numbers: ";
+    cin >> x >> y;
+    sum(x,y);
+    cout << endl;
+    sub(x,y);
+    return 0;
+}
+void sum(int x, int y) {
+    cout << "Summition of the 2 numbers = " << x + y;
+}
+void sub(int x, int y) {
+    cout << "Subtraction of the 2 numbers = " << x - y;
 }
 
 
